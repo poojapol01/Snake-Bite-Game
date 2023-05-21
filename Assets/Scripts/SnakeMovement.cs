787679e989e0a6c9b08a1b40e08f530afeaa9dd5
poojapol01 <polpooja05@gmail.com>
2023-05-21 23:27:31 +0530

    Added Snake Movement with Keys

---
+++ Assets/Scripts/SnakeMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //this.transform.position.x + _direction.
+            _direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            _direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            _direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _direction = Vector2.right;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + _direction.x,
+            Mathf.Round(this.transform.position.y) + _direction.y,
+            0.0f);
+    }
+}
