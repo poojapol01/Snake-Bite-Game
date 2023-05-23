043fe2031bfb28fd43009403efa8a022a4fa96f2
poojapol01 <polpooja05@gmail.com>
2023-05-23 23:37:23 +0530

    Added Game Over States

--- Assets/Scripts/SnakeMovement.cs
+++ Assets/Scripts/SnakeMovement.cs
@@ -55,6 +55,10 @@ public class SnakeMovement : MonoBehaviour
         {
             Grow();
         }
+        if (collision.tag == "Obstacle")
+        {
+            ResetState();
+        }
     }
 
     private void Grow()
@@ -62,5 +66,16 @@ public class SnakeMovement : MonoBehaviour
         Transform segment = Instantiate(this.snakeBodyPrefab);
         segment.position = _segments[_segments.Count - 1].position;
         _segments.Add(segment);
+    }
+
+    private void ResetState()
+    {
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+        _segments.Clear();
+        _segments.Add(this.transform);
+        this.transform.position = Vector3.zero;
     }
 }
