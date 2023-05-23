40a5cd57262ecd222841af6a021139280dadc646
poojapol01 <polpooja05@gmail.com>
2023-05-23 23:04:58 +0530

    Added Snake Body growing Segments

--- Assets/Scripts/SnakeMovement.cs
+++ Assets/Scripts/SnakeMovement.cs
@@ -1,8 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SnakeMovement : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
+
+    public Transform snakeBodyPrefab;
+
+    private List<Transform> _segments;
+
+    private void Start()
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+    }
 
     private void Update()
     {
@@ -27,9 +38,29 @@ public class SnakeMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for(int i=_segments.Count-1 ; i > 0 ; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + _direction.x,
             Mathf.Round(this.transform.position.y) + _direction.y,
             0.0f);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Food")
+        {
+            Grow();
+        }
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.snakeBodyPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
     }
 }
